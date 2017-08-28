1393bbc745300a4429aafbb785b770ef90f3dee0
felix <felixurbina@gmx.com>
2017-08-27 23:31:10 -0500

    py1

--- primes.py
+++ primes.py
@@ -1,16 +1,21 @@
-"""
-Your module documentation here
-"""
+#!/usr/bin/env python
+#-*- encoding: UTF-8 -*-
 
+import math
 
 class PrimeClass(object):
+    
+    def is_prime(num_int):
+        if num_int%2 == 0:
+            return False
+        else:
+            return True
+        
     """
-    Your class documentation here
+        Validate the number before operation
     """
-
-    def is_prime(self, num_int):
-        """
-        Your method documentation here
-        """
-        # your primes code here
-        return "not implement yet"  # Remove this dummy line
+    n = input("Introduce un número entero: ")    
+    if type(n) == int and n > 2:
+        is_prime(n)
+    else:
+        print "Es flotante o uno (1), realice nuevamente la operación"
